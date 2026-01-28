8c6b7c4aa64c27ccae7c9b5fdedd438c440bd5bb
Ken Greim <34892440+kengreim@users.noreply.github.com>
2026-01-28 15:58:56 -0800

    Add lz4 compression to initial migration

--- backend/migrations/20260128151045_create_datafeeds_table.sql
+++ backend/migrations/20260128151045_create_datafeeds_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE datafeeds
 (
     id               BIGSERIAL PRIMARY KEY,
     update_timestamp TIMESTAMPTZ NOT NULL UNIQUE,
-    pilots           JSONB       NOT NULL
+    pilots           JSONB       NOT NULL COMPRESSION lz4
 );
 
 CREATE INDEX idx_datafeeds_update_timestamp ON datafeeds (update_timestamp);
