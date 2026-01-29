aa909e28692295382d33bf3d26e9a02e506af3e9
Ken Greim <34892440+kengreim@users.noreply.github.com>
2026-01-28 16:19:33 -0800

    Fix sql order in migration

--- backend/migrations/20260128151045_create_datafeeds_table.sql
+++ backend/migrations/20260128151045_create_datafeeds_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE datafeeds
 (
     id               BIGSERIAL PRIMARY KEY,
     update_timestamp TIMESTAMPTZ NOT NULL UNIQUE,
-    pilots           JSONB       NOT NULL COMPRESSION lz4
+    pilots           JSONB       COMPRESSION lz4 NOT NULL
 );
 
 CREATE INDEX idx_datafeeds_update_timestamp ON datafeeds (update_timestamp);
